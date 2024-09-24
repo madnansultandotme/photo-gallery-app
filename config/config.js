ffc0551d5819553f46eaa469fe1f07c33a40fb12
Muhammad Adnan Sultan <info.adnansultan@gmail.com>
2024-09-24 11:14:59 +0500

    Changed the Database

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    DB_HOST: 'localhost',
-    DB_USER: 'root',
-    DB_PASSWORD: '',
+    DB_HOST: 'https://auth-db1274.hstgr.io/',
+    DB_USER: 'u481673204_photogallarydb',
+    DB_PASSWORD: 'l6Gzx4+N??',
     DB_NAME: 'photogallarydb'
   };
   
