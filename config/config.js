a5bec9788649059b4ae3a23bc4392135d8710782
Muhammad Adnan Sultan <info.adnansultan@gmail.com>
2024-09-24 11:32:57 +0500

    Updated Version

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    DB_HOST: 'https://auth-db1274.hstgr.io/',
-    DB_USER: 'u481673204_photogallarydb',
+    DB_HOST: 'srv1274.hstgr.io',
+    DB_USER: 'u481673204_madnansultan',
     DB_PASSWORD: 'l6Gzx4+N??',
-    DB_NAME: 'photogallarydb'
+    DB_NAME: 'u481673204_photogallarydb'
   };
   
--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const config = require('./config');
 
+// Create a MySQL connection pool
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: config.DB_HOST,
@@ -9,6 +10,18 @@ const pool = mysql.createPool({
   database: config.DB_NAME
 });
 
+// Promisify pool queries
 const promisePool = pool.promise();
 
+// Test the connection when starting the app
+promisePool.getConnection()
+  .then((connection) => {
+    console.log('DB Connected');
+    connection.release(); // Release the connection back to the pool
+  })
+  .catch((err) => {
+    console.error('Error connecting to DB:', err.message);
+  });
+
 module.exports = promisePool;
+
